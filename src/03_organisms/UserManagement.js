d5a21e6b6b56144729fb620760e19cd0f1258a79
Tobias Gerstenberg <tobigers@gmail.com>
2019-05-31 14:50:17 +0200

    ADDED testrendering for a single user in usermanagement for admins

--- src/03_organisms/UserManagement.js
+++ src/03_organisms/UserManagement.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Trans, withTranslation } from "react-i18next";
-import { Field, reduxForm } from "redux-form";
-import { Button, Icon, Table } from "semantic-ui-react";
+import { Button, Icon, Table, Modal } from "semantic-ui-react";
 import { userActions } from "../redux/_actions";
+import UpdateProfileForm from "./UpdateProfileForm";
+import tableFormattingUtilities from "../utilities/tableFormattingUtilities";
 
 class UserManagement extends React.Component {
   componentWillMount() {
@@ -21,34 +22,27 @@ class UserManagement extends React.Component {
   }
 
   renderUsersTable(users) {
+    console.log(users);
     return (
-      <Table celled compact definition selectable>
-        <Table.Header fullWidth>
-          <Table.Row>
-            <Table.HeaderCell />
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Registration Date</Table.HeaderCell>
-            <Table.HeaderCell>E-mail address</Table.HeaderCell>
-            <Table.HeaderCell>Premium Plan</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
+      <Table celled selectable>
+        {this.renderUsersTableHeader()}
         <Table.Body>
-          {users.forEach((user, index) => {
+          {this.renderUsersTableRow(users[0])}
+          {users.forEach(user => {
             this.renderUsersTableRow(user);
           })}
         </Table.Body>
 
         <Table.Footer fullWidth>
           <Table.Row>
-            <Table.HeaderCell />
-            <Table.HeaderCell colSpan="4">
+            <Table.HeaderCell colSpan="14">
               <Button
                 floated="right"
                 icon
                 labelPosition="left"
                 primary
                 size="small"
+                onClick={this.handleAddUserButtonClick}
               >
                 <Icon name="user" />
                 <Trans i18nKey="userManagement-add-user-button" />
@@ -82,28 +76,141 @@ class UserManagement extends React.Component {
     );
   }
 
+  renderUsersTableHeader() {
+    return (
+      <Table.Header fullWidth>
+        <Table.Row>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-id" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-email" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-password" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-title" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-gender" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-firstName" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-lastName" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-birthDate" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-city" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-postCode" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-street" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-studentStatus" />
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            <Trans i18nKey="usermanagement-tableheader-employeeStatus" />
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+    );
+  }
+
   renderUsersTableRow(user) {
-    const TableRow = user => (
-      <Table.Row>
+    console.log(user);
+    return (
+      <Table.Row
+        key={"row" + user.id}
+        onClick={() => {
+          this.setState({ selectedEntry: user.id });
+        }}
+      >
+        <Table.Cell key="id">{user.id}</Table.Cell>
         <Table.Cell key="email">{user.email}</Table.Cell>
         <Table.Cell key="password">{user.password}</Table.Cell>
+        <Table.Cell key="title">{user.title}</Table.Cell>
+        <Table.Cell key="gender">{user.gender}</Table.Cell>
         <Table.Cell key="firstName">{user.firstName}</Table.Cell>
         <Table.Cell key="lastName">{user.lastName}</Table.Cell>
         <Table.Cell key="birthDate">{user.birthDate}</Table.Cell>
-        <Table.Cell key="city">{user.adress.city}</Table.Cell>
-        <Table.Cell key="postCode">{user.adress.postCode}</Table.Cell>
-        <Table.Cell key="street">{user.adress.street}</Table.Cell>
-        <Table.Cell key="houseNumber">{user.adress.houseNumber}</Table.Cell>
-        <Table.Cell key="isStudent">{user.studentStatus}</Table.Cell>
-        <Table.Cell key="isEmployee">{user.employeeStatus}</Table.Cell>
+        <Table.Cell key="city">{user.address.city}</Table.Cell>
+        <Table.Cell key="postCode">{user.address.postCode}</Table.Cell>
+        <Table.Cell key="street">{user.address.street}</Table.Cell>
+        <Table.Cell key="isStudent">
+          {tableFormattingUtilities.stringValueForBoolean(user.studentStatus)}
+        </Table.Cell>
+        <Table.Cell key="isEmployee">
+          {tableFormattingUtilities.stringValueForBoolean(user.employeeStatus)}
+        </Table.Cell>
       </Table.Row>
     );
-    return TableRow;
+  }
+
+  handleAddUserButtonClick() {
+    this.openModalUserForm();
+  }
+
+  openModalUserForm() {
+    return (
+      <Modal>
+        <UpdateProfileForm />
+      </Modal>
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  users: state.user.users,
+  users: [
+    {
+      id: 1,
+      firstName: "Sebastian",
+      lastName: "Zilles",
+      title: null,
+      gender: 1,
+      birthDate: "1994-01-23T00:00:00",
+      address: {
+        id: 1,
+        name: null,
+        street: "Am Gänschenwald 15",
+        postCode: "51467",
+        city: "Bergisch Gladbach",
+        room: "Example",
+        phoneNumber: "01605000231",
+        phoneNumberMobile: null,
+        email: null
+      },
+      studentStatus: {
+        id: 0,
+        personForeignId: 0,
+        person: null,
+        matriculationNumber: null,
+        matriculationDate: "0001-01-01T00:00:00",
+        exmatriculationDate: "0001-01-01T00:00:00",
+        subject: null
+      },
+      chairs: [
+        {
+          personId: 1,
+          chairId: 1,
+          chair: null,
+          role: 2,
+          active: false,
+          chairAdmin: false
+        }
+      ],
+      chairSubscriptions: [],
+      authoredChairPosts: [],
+      skills: []
+    }
+  ],
   ...state
 });
 
